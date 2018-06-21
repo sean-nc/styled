3eac845b104b398966f082ada0c173464862df7c
Sean <sean.n.cordes@gmail.com>
2018-06-21 14:09:28 -0400

    added page titles helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns title each page
+  def full_title(page_title = '')
+    base_title = "Styled"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end

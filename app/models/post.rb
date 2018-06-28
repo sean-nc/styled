dea5336052d2bf98dba719c522a958eea881ec3c
Sean <sean.n.cordes@gmail.com>
2018-06-28 14:29:58 -0400

    updated post search

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,25 +13,29 @@ class Post < ApplicationRecord
     if term
       self.where('description ILIKE ?', "%#{term}%").order("random()")
 
-      # self.select('*').joins(:clothing_articles).select("CASE
-      #                         WHEN posts.description ILIKE '#{term}' THEN 2
-      #                         WHEN clothing_articles.title ILIKE '#{term}' THEN 1
-      #                         WHEN clothing_articles.description ILIKE '#{term}' THEN 1
-      #                         ELSE 0 END as score").order('score DESC')
+      # self.select('*')
+      #     .where('posts.description ILIKE ?', "%#{term}%")
+      #     .left_outer_joins(:clothing_articles)
+      #     .select("CASE
+      #             WHEN clothing_articles.title ILIKE '%#{term}%' THEN 1
+      #             WHEN clothing_articles.description ILIKE '%#{term}%' THEN 1
+      #             ELSE 0 END as score")
+      #     .order('score DESC')
 
-        # query = <<-QUERY.squish
-        # SELECT *, CASE
-        #   WHEN posts.description ILIKE '#{term}' THEN 2
-        #   WHEN clothing_articles.title ILIKE '#{term}' THEN 1
-        #   WHEN clothing_articles.description ILIKE '#{term}' THEN 1
-        #   ELSE 0
-        #   END
-        # AS score
-        # JOIN clothing_articles
-        # ORDER_BY score
-        # QUERY
+      # query = <<-QUERY.squish
+      # SELECT *, CASE
+      #   WHEN clothing_articles.title ILIKE :term THEN 1
+      #   WHEN clothing_articles.description ILIKE :term THEN 1
+      #   ELSE 0
+      #   END
+      # AS score
+      # FROM "posts"
+      # LEFT OUTER JOIN clothing_articles ON clothing_articles.post_id = posts.id
+      # WHERE (posts.description ILIKE :term)
+      # ORDER BY score DESC
+      # QUERY
 
-        # self.find_by_sql([query, :term => term])
+      # find_by_sql([query, :term => term])
     else
       self.all.order("random()")
     end

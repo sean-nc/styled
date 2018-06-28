fdfb0274b9a4e659fff35f90c3798f2471ae55c3
Sean <sean.n.cordes@gmail.com>
2018-06-28 10:52:58 -0400

    working on post search

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,27 @@ class Post < ApplicationRecord
 
   def self.search(term)
     if term
-      self.where('description ILIKE ?', "%#{term}%")
+      self.where('description ILIKE ?', "%#{term}%").order("random()")
+
+      # self.select('*').joins(:clothing_articles).select("CASE
+      #                         WHEN posts.description ILIKE '#{term}' THEN 2
+      #                         WHEN clothing_articles.title ILIKE '#{term}' THEN 1
+      #                         WHEN clothing_articles.description ILIKE '#{term}' THEN 1
+      #                         ELSE 0 END as score").order('score DESC')
+
+        # query = <<-QUERY.squish
+        # SELECT *, CASE
+        #   WHEN posts.description ILIKE '#{term}' THEN 2
+        #   WHEN clothing_articles.title ILIKE '#{term}' THEN 1
+        #   WHEN clothing_articles.description ILIKE '#{term}' THEN 1
+        #   ELSE 0
+        #   END
+        # AS score
+        # JOIN clothing_articles
+        # ORDER_BY score
+        # QUERY
+
+        # self.find_by_sql([query, :term => term])
     else
       self.all.order("random()")
     end

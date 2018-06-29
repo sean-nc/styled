f32fc1767938b5fea41dabfbacdf6ab74944bab9
Sean <sean.n.cordes@gmail.com>
2018-06-28 23:45:36 -0400

    updated bootstrap, added some styling to form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,19 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+
+$(function() {
+  $('#pictureInput').on('change', function(event) {
+    var files = event.target.files;
+    var image = files[0]
+    var reader = new FileReader();
+    reader.onload = function(file) {
+      var img = new Image();
+      console.log(file);
+      img.src = file.target.result;
+      $('#target').html(img);
+    }
+    reader.readAsDataURL(image);
+    console.log(files);
+  });
+});
